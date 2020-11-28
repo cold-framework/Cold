dfa48ca7791086cdaf65ee709ed2f4db6d39830a
Superkooka <35631093+Superkooka@users.noreply.github.com>
2020-11-28 15:57:53 +0100

    feat: icon is changing with the current state of section fix: move fa --> bi

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,6 +1,3 @@
-let success = "fa fa-check-circle text-green-600";
-let error = "fas fa-circle-notch text-red-600";
-
 let sections = [
   {
     $icon: document.getElementById("icon-date"),
@@ -89,8 +86,32 @@ let sections = [
   },
 ];
 
+let error_icon = `
+<svg width="1em" height="1em" viewBox="0 0 16 16" fill="rgb(220, 38, 38)" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+</svg>
+`;
+
+let sucess_icon = `
+<svg width="1em" height="1em" viewBox="0 0 16 16" fill=" rgb(5, 150, 105)" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+</svg>
+`;
+
+let info_icon = `
+<svg width="1em" height="1em" viewBox="0 0 16 16" fill="rgb(37, 99, 235)" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.93 4.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+</svg>
+`;
+
+document.getElementById("icon-info").innerHTML = info_icon;
+
 sections.forEach((section) => {
   Object.keys(section.child).forEach((key) => {
+    section.$icon.innerHTML = isSectionValid(section)
+      ? sucess_icon
+      : error_icon;
+
     section.child[key].$el.addEventListener("focusout", () => {
       if (!section.child[key].state.focusable) {
         section.child[key].state.focusable = true;
@@ -98,15 +119,21 @@ sections.forEach((section) => {
       }
       section.child[key].state.valid = section.child[key].$el.reportValidity();
 
-      let section_is_valid = true;
-      Object.keys(section.child).forEach(($input) => {
-        if (!section.child[$input].$el.checkValidity()) {
-          section_is_valid = false;
-        }
-      });
-
-      section.$icon.classList = section_is_valid ? success : error;
+      section.$icon.innerHTML = isSectionValid(section)
+        ? sucess_icon
+        : error_icon;
       section.child[key].state.focusable = false;
     });
   });
 });
+
+function isSectionValid(section) {
+  let section_is_valid = true;
+  Object.keys(section.child).forEach(($input) => {
+    if (!section.child[$input].$el.checkValidity()) {
+      section_is_valid = false;
+    }
+  });
+
+  return section_is_valid;
+}
